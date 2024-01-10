046998b7720bc805ddd208a25cd73645e30d94bd
Efim <efimmanevich@gmail.com>
2024-01-10 22:30:43 +0300

    texture

--- sdff.py
+++ sdff.py
@@ -85,41 +85,49 @@ def sphere_projection(map, fname, nx = 100, ny = 100, r = 2., osd = 1):
     
 
 
-def param_surf(map, fname, nx, ny, x0, x1 ,y0 , y1, norm = True ):
-    sys.stdout = open(fname, "w")
-    nvert = 0
-    for _ in range(1):
-        vert = [0]*(nx*ny + 1)
-        for i in range(nx):
-            for j in range(ny):
-                x = x0 + (x1-x0)*i/(nx - 1)
-                y = y0 + (y1-y0)*j/(ny - 1)
-                d = map(x, y)
+def param_surf(map, fname, nx, ny, x0, x1 ,y0 , y1, txr = None, nvert = 0):
+    if fname!="":
+        sys.stdout = open(fname, "w")
+    
+    vert = [0]*(nx*ny + 1)
+    for i in range(nx):
+        for j in range(ny):
+            x = x0 + (x1-x0)*i/(nx-1)
+            y = y0 + (y1-y0)*j/(ny-1)
+            d = map(x, y)
+            if d:
                 print(d[0])
                 print(d[1].vn())
+                if (txr):
+                    print(txr(x, y))
                 nvert += 1
                 vert[i*ny + 1 + j] = nvert
 
-        for i in range(nx-1):
-            for j in range(ny-1):
-                a = i*ny + 1 + j
-                b = a + 1
-                c = a+ny
-                d = b + ny
-                
-                a = vert[a]
-                b = vert[b]
-                c = vert[c]
-                d = vert[d]
+    for i in range(nx-1):
+        for j in range(ny-1):
+            a = i*ny + 1 + j
+            b = a + 1
+            c = a+ny
+            d = b + ny
+            
+            a = vert[a]
+            b = vert[b]
+            c = vert[c]
+            d = vert[d]
 
-                if (norm):
-                    if (a *c * b > 0):
-                        print(f'f {a}//{a} {c}//{c} {b}//{b}')
-                    if (b *c * d):
-                        print(f'f {b}//{b} {c}//{c} {d}//{d}')
-                else:        
-                    print(f'f {a} {c} {b}')
-                    print(f'f {b} {c} {d}')
+            if (not txr):
+                if (a *c * b > 0):
+                    print(f'f {a}//{a} {c}//{c} {b}//{b}')
+                if (b *c * d):
+                    print(f'f {b}//{b} {c}//{c} {d}//{d}')
+            else:
+                if (a *c * b > 0):
+                    print(f'f {a}/{a}/{a} {c}/{c}/{c} {b}/{b}/{b}')
+                if (b *c * d):
+                    print(f'f {b}/{b}/{b} {c}/{c}/{c} {d}/{d}/{d}')        
+                #print(f'f {a} {c} {b}')
+                #print(f'f {b} {c} {d}')
+    return nvert
 
 
 #calculte normal and points on curve
@@ -132,23 +140,35 @@ def curve_norm(t, f, r, curve, deriv = None):
         vth = curve(t+h)
         r1 = normalize(vth - vt)
         r2 = normalize(curve(t+2*h) - vth*2. + vt)
-    x = cross(r1, r2)
-    y = cross(x, r1)
-    if length(x) < 1 or length(y) < 1:
-        x = vec3(1, 0, 0)
-        y = vec3(0, 1, 0)
-
+    
+    x = normalize(cross(r1, r2))
+    y = normalize(cross(x, r1))
     nor = x*cos(f) + y*sin(f)
     val = curve(t) + nor*r
     return (val, nor)
 
 
+#calculte normal and points on curve
+def curve_norm2(t, f, r, curve):
+    h = 0.0001
+    vt = curve(t)
+    vth = curve(t+h)
+    r1 = normalize(vth - vt)
+    x = normalize(cross(r1, vec3(0, 0, 1)))
+    if length(x) == 0:
+        x = vec3(1, 0, 0)
+        y = vec3(0, 1, 0)
+    else:
+        y = normalize(cross(x, r1))    
+    
+    nor = x*cos(f) + y*sin(f)
+    val = curve(t) + nor*r
+    return (val, nor)
+
+#calculate normal to surface
 def normal_surf(u, v, surf):
     h = 0.0001
-    f = surf(u, v)
     du = surf(u+h, v) - surf(u-h, v)
     dv = surf(u, v+h) - surf(u, v-h)
-    x = du.y * dv.z - dv.y * du.z
-    y = du.z * dv.x - dv.z * du.x
-    z = du.x * dv.y - dv.x * du.y
-    return normalize(vec3(x, y, z))
+    return normalize(cross(du, dv))
+
--- tor.py
+++ tor.py
@@ -1,4 +1,4 @@
-from math import sin, cos
+from math import sin, cos, exp
 from  glslf  import *
 from sdff import *
 
@@ -27,17 +27,45 @@ def cosn(u, v):
     nor = normal_surf(u, v, cosn2)
     return (val, nor)
 
-
-
+#https://mathcurve.com/surfaces.gb/coquillage/coquillage.shtml
+def seashell (u, v):
+    a = 3
+    b = 2.5
+    m = -0.1
+    k = 2.5
+    x = exp(m*u)*cos(u)*(a + b*cos(v))
+    y = exp(m*u)*sin(u)*(a + b*cos(v))
+    z = exp(m*u)*(k*a + b*sin(v))
+    return vec3(x, y, z)
 
     
+def seashell_map(u, v):
+    val = seashell(u, v)
+    nor = normal_surf(u, v, seashell)
+    return (val, nor)
 
+def texture0(u, v):
+    u = (u % TAU)/(TAU + 0.1)
+    v = (v % TAU)/(TAU + 0.1)
+    s = f'vt {u} {v}'
+    return s
+
+
+#https://mathcurve.com/surfaces.gb/sinus/sinus.shtml
+def sine(u, v):
+    a = 2.
+    return vec3(a*sin(u), a*sin(v), a*sin(u+v))
+
+def sine_map(u, v):
+    val = sine(u, v)
+    nor = normal_surf(u, v, sine)
+    return (val, nor)
 
 def trefoil_knot(t):
     return vec3(sin(t) + 2*sin(2*t), cos(t) - 2*cos(2*t), -0.5*sin(3*t))
 
 def trefoil(t, f):
-    return curve_norm(t, f, 0.3, trefoil_knot)
+    return curve_norm2(t, f, 0.3, trefoil_knot)
 
 def eight_knot(t):
     return vec3((2 + cos(2*t))*cos(3*t), (2 + cos(2*t))*sin(3*t), 0.5*sin(4*t))
@@ -53,7 +81,7 @@ def sinwave(t):
     return vec3(a*cos(t), a*sin(t), b*sin(n/m*t))
 
 def sinwave_map(t, f):
-    return curve_norm(t, f, 0.1, sinwave)
+    return curve_norm2(t, f, 0.1, sinwave)
 
 def rose(t):
     a = 3.
@@ -72,7 +100,20 @@ def clelia(t):
     return vec3(a*cos(t), a*sin(t), r*sin(n*t))
 
 def clelia_map(t, f):
-    return curve_norm(t, f, 0.2, clelia)
+    return curve_norm2(t, f, 0.3, clelia)
+
+#https://mathcurve.com/courbes3d.gb/couture/couture.shtml
+def tennis(t):
+    a = 3.
+    b = 2.5
+    c = 2 * math.sqrt(a*b)
+    x = a*cos(t) + b*cos(3*t)
+    y = a*sin(t) - b*sin(3*t)
+    z = c * sin(2*t)
+    return vec3(x, y, z)
+
+def tennis_map(t, f):
+    return curve_norm(t, f, 0.4, tennis)
 
 
 #https://mathcurve.com/courbes3d.gb/lissajous3d/lissajous3d.shtml
@@ -101,16 +142,30 @@ def liss_deriv(t):
 
 def liss_map(t, f):
     #return curve_norm(t, f, 0.25, liss, liss_deriv)
-    return curve_norm(t, f, 0.2, liss)
+    return curve_norm2(t, f, 0.2, liss)
 
 #param_surf(map, "torp1.obj", 100, 100, 0., TAU , 0., TAU)
 #param_surf(cosn, "cosn2.obj", 100, 100, 0., TAU , 0., PI/4)
-#param_surf(trefoil, "trfoil.obj", 100, 100, 0., TAU , 0., TAU)
-#param_surf(eight, "eight_knot.obj", 200, 100, 0., TAU , 0., TAU)
-#param_surf(sinwave_map, "sinwave.obj", 700, 50, 0., 7.*TAU , 0., TAU)
-#param_surf(rose_map, "rose3.obj", 500, 50, 0., 5.*TAU , 0., TAU)
-#param_surf(clelia_map, "clelia1.obj", 100, 100, 0., TAU , 0., TAU)
-#param_surf(liss_map, "liss.obj", 700, 100, 0., 7.*TAU , 0., TAU)
+#param_surf(trefoil, "trfoil22.obj", 100, 100, 0., TAU , 0., TAU)
+#param_surf(eight, "eight_knot1.obj", 200, 100, 0., TAU , 0., TAU)
+#param_surf(sinwave_map, "sinwave2.obj", 700, 50, 0., 7.*TAU , 0., TAU)
+#param_surf(rose_map, "rose33.obj", 500, 100, 0., 5.*TAU , 0., TAU)
+#param_surf(clelia_map, "clelia2.obj", 100, 100, 0., TAU , 0., TAU)
+#param_surf(liss_map, "liss22.obj", 700, 100, 0., 7.*TAU , 0., TAU)
+param_surf(seashell_map, "seashell4.obj", 700, 100, 0., 7.*TAU , 0., TAU, texture0)
+#param_surf(sine_map, "sine22.obj", 100, 100, 0., TAU , 0., TAU)
+
+#n1 = param_surf(trefoil, "combo.obj", 200, 100, 0., TAU , 0., TAU)
+#param_surf(clelia_map, "", 200, 100, 0., TAU , 0., TAU, n1)
+
+#param_surf(tennis_map, "tennis5.obj", 200, 50, 0., TAU , 0., TAU)
+
+
+
+
+
+
+
 
 
 def scale5(x, y):
@@ -158,4 +213,4 @@ def pine_map(u, v):
     return (val, nor)
 
 
-param_surf(pine_map, "pine2.obj", 100, 300, 0., TAU , 0., PI/4)
+#param_surf(pine_map, "pine2.obj", 100, 300, 0., TAU , 0., PI/4)
