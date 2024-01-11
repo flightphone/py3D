7dca9f7a9f5c0c835a71353085b0585c06e8e6dd
Efim <efimmanevich@gmail.com>
2024-01-12 02:42:56 +0300

    solenoid

--- curves.py
+++ curves.py
@@ -2,23 +2,23 @@ from math import sin, cos, exp
 from  glslf  import *
 from sdff import *
 
-h = 3
+h = 5
 axis_x = vec3(1, 0, 0)
 axis_y = vec3(0, 1, 0)
 axis_z = vec3(0, 0, 1)
 shift = vec3(0, 0, 0)
 
 def heir(t):
-    fi = 0.8
+    fi = 0.8#dot(axis_x, vec3(1.12, 3, 6))
     z = t
-    x = sin(z * TAU  + fi) * h * 0.6 * z * (h - z) / h / h 
-    y = sin(z * TAU  - fi) * h * 0.6 * z * (h - z) / h / h
+    x = sin(z * TAU  + fi) * h * 0.3 * z * (h - z) / h / h 
+    y = sin(z * TAU  - fi) * h * 0.3 * z * (h - z) / h / h
     return axis_x*x + axis_y*y + axis_z*z + shift
 
 def heir_map(t, f):
     
-    r1 = 0.08
-    r0 = 0.02
+    r1 = 0.1
+    r0 = 0.04
     if t <= h and t >= 0:
         res =  curve_norm(t, f, 0., heir)   
         r = r1 * (h - t) / h + r0 
@@ -51,23 +51,27 @@ def sphere(u, v):
     return(val, nor)
 
 #param_surf(heir_map, "heir.obj", 100, 40, -1., h+1, 0., TAU, texture1)
-nvert = param_surf(sphere, "virus.obj", 100, 100, 0., TAU, 0., PI, texture0)
-n = 5
-for i in range(n):
-    fi = TAU*i/n
-    a = vec3(sin(5*PI/6)*cos(fi), sin(5*PI/6)*sin(fi), cos(5*PI/6))
-    axis_x, axis_y, axis_z = getAxis(a)
-    shift = a*2
-    nvert = param_surf(heir_map, "", 200, 100, -1., h+1, 0., TAU, texture1, nvert)
-'''
-float heirw(vec3 p, float h, float r, float r0, float fi) {
-    h = h * (1.0 + 0.05 * cos(iTime * 4. + fi));
-    float z = clamp(p.z, 0., h), // radius pimple 
-    x = sin(z * PI * 2.0 + iTime * 4. + fi) * h * 0.3 * z * (h - z) / h / h, y = sin(z * PI * 2.0 - iTime * 4. + fi) * h * 0.3 * z * (h - z) / h / h;
+def render_sprut():
+    nvert = param_surf(sphere, "sprut.obj", 100, 100, 0., TAU, 0., PI, texture0)
+    n = 5
+    global axis_x, axis_y, axis_z, shift
+    for i in range(n):
+        fi = TAU*i/n
+        a = vec3(sin(5*PI/6)*cos(fi), sin(5*PI/6)*sin(fi), cos(5*PI/6))
+        axis_x, axis_y, axis_z = getAxis(a)
+        shift = a*2
+        nvert = param_surf(heir_map, "", 100, 50, -1., h+1, 0., TAU, texture1, nvert)
 
-    vec3 p2 = vec3(x, y, z);
-    //Color
-    sdfColor = mix(col1, col2, pow(vec3(p.z / h), vec3(3.)));
-    return length(p - p2) * 0.5 - r * (h - z) / h - r0;
-}
-'''
+#render_sprut()
+#https://mathcurve.com/courbes3d.gb/solenoidtoric/solenoidtoric.shtml
+def solenoid(t):
+    n = 1.25
+    R = 3
+    r = 1
+    return vec3((R + r*cos(n*t))*cos(t), (R + r*cos(n*t))*sin(t), r*sin(n*t))
+
+def solenoid_map(t, f):
+    return(curve_norm(t, f, 0.2, solenoid))
+
+param_surf(solenoid_map, "solenoid.obj", 500, 100, 0., 4*TAU, 0., TAU)
+#param_surf(heir_map, "heir.obj", 400, 40, -1., h+1, 0., TAU, texture1)
